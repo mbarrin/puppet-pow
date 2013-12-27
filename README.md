aade5831110b6b73cf8a405d9f3ebcbc61a2ccc7
Juan Ignacio Donoso <jidonoso@gmail.com>
2013-12-27 11:26:40 -0300

    Adds enhanced compatibility with boxen

--- README.md
+++ README.md
@@ -6,9 +6,37 @@ Installs Pow, a simple app server from 37 Signals.
 
 http://pow.cx
 
+## Usage
+
+``` puppet
+include pow
+
+# include with some custom params
+# this example shows the default values
+class {'pow':
+      host_dir => '/opt/boxen/data/pow/hosts',
+      log_dir => '/opt/boxen/logs/pow',
+      dns_port => 30560,
+      http_port => 30559,
+      dst_port => 1999,
+      domains => 'pow',
+      ext_domains => undef,     # uses pow default, ""
+      timeout => undef,         # uses pow default, 900
+      workers => undef,         # uses pow default, 2
+      nginx_proxy => true
+    }
+```
+
+The pow module installs pow with a custom `destination port` and use `nginx` to proxy custom domains to pow, instead of a firewall rule. You can change this behaviour with the `nginx_proxy` parameter
+
+It also use the `.pow` TLD that resolve to `127.0.0.0` to serve your pow projects
+
 ## Required Puppet Modules
 
 * `boxen`
+* `nginx`
+* `homebrew`
+* `stdlib`
 
 ## Development
 
--- manifests/init.pp
+++ manifests/init.pp
@@ -3,9 +3,116 @@
 # Usage:
 #
 #     include pow
-class pow {
-  package { 'pow':
-    ensure   => 'latest',
-    provider => 'homebrew'
-  }
+class pow(
+  $host_dir = $pow::config::host_dir,
+  $log_dir = $pow::config::log_dir,
+  $dns_port = $pow::config::dns_port,
+  $http_port = $pow::config::http_port,
+  $dst_port = $pow::config::dst_port,
+  $domains = $pow::config::domains,
+  $ext_domains = undef,
+  $timeout = undef,
+  $workers = undef,
+  $nginx_proxy = true,
+
+) inherits pow::config {
+    include boxen::config
+    include homebrew::config
+
+    # Current user
+    $current_user = $::boxen_user
+
+    # Pow root
+    $pow_dir = regsubst($host_dir, '\/hosts$', '')
+
+    # Pow executable
+    $pow_bin = "${homebrew::config::installdir}/bin/pow"
+
+    $home = "/Users/${::boxen_user}"
+    file { "${home}/.powconfig":
+      ensure  => present,
+      content => template('pow/powconfig.erb'),
+      mode    => '0644'
+    }
+
+    # Install our custom plist for pow.
+    # NOTE: Puppet launchd service provider cannot manage
+    # per-user setted by user in ~/Library/LaunchAgents
+    # We will set the pow daemon in the per-user set by
+    # administrator location
+    file { "/Library/LaunchAgents/dev.pow.powd.plist":
+      content => template('pow/dev.pow.powd.plist.erb'),
+      notify  => Service['dev.pow.powd'],
+      group   => 'wheel',
+      owner   => 'root'
+    }
+
+    # Install pow with brew
+    package { 'pow':
+      ensure   => 'latest',
+      provider => 'homebrew',
+      require  => File["${home}/.powconfig"]
+    }
+
+    # Create the required host directories:
+    file { [
+        $pow_dir,
+        $host_dir,
+        $log_dir
+        ]:
+        ensure => directory
+    }
+
+    # Create the symbolic link to hosts
+    file { "${home}/.pow":
+        ensure  => link,
+        target  => $host_dir,
+        require => File[$host_dir],
+    }
+
+    # Use the nginx proxy on port 80
+    if $nginx_proxy {
+        include nginx::config
+        include nginx
+
+        # Create the site with a proxy from port 80 to $http_port
+        file { "${nginx::config::sitesdir}/pow.conf":
+            content => template('pow/nginx/pow.conf.erb'),
+            require => File[$nginx::config::sitesdir],
+            notify  => Service['dev.nginx'],
+        }
+    }
+    # Create a firewall rule to redirect from $dst_port to $http_port
+    else{
+      # Install our custom plist for pow firewall.
+      file { '/Library/LaunchDaemons/dev.pow.firewall.plist':
+        content => template('pow/dev.pow.firewall.plist.erb'),
+        group   => 'wheel',
+        notify  => Service['dev.pow.firewall'],
+        owner   => 'root'
+      }
+
+      # Start the pow firewall service
+      service { 'dev.pow.firewall':
+        ensure  => running,
+        require => Package['pow']
+      }
+    }
+
+    # Start the pow service
+    service { 'dev.pow.powd':
+      ensure  => running,
+      require => Package['pow']
+    }
+
+    # Add the dns resolver for each domain
+    $pow_domains = split(strip($domains), ',')
+    $pow_domain_resolvers = prefix($pow_domains, '/etc/resolver/')
+
+    file { $pow_domain_resolvers:
+      content => template('pow/resolver.erb'),
+      group   => 'wheel',
+      owner   => 'root',
+      require => File['/etc/resolver']
+    }
 }
